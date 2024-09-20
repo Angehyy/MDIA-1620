d4cbe1b669d9c754bdd7c0ba9c5ae88e3c17ecea
Angel Hsieh <angelhsieh@Angels-MacBook-Pro-2.local>
2024-09-20 15:49:27 -0700

    adding files and stables

---
+++ stables.js
@@ -0,0 +1,9 @@
+let horseName = "HorseMeat";
+console.log(horseName);
+console.log(horseName);
+let horseAge = 420;
+console.log(horseAge);
+let horseHairColour = "white";
+console.log(horseHairColour);
+let isHorseInside = true;
+console.log(isHorseInside);
