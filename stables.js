324d90d1ae97a18677669b5df2c56c96a17cd609
Angel <angelhsieh@Angels-MacBook-Pro-2.local>
2024-09-27 16:21:40 -0700

    Lab 4 work

--- stables.js
+++ stables.js
@@ -1,6 +1,8 @@
 //LESSON-3 Code
 let horseName = "HorseMeat";
 console.log(horseName);
+let horseNickName = "Vegan"
+console.log(horseNickName);
 let horseAge = 420;
 console.log(horseAge);
 let isHorseInside = true;
@@ -8,7 +10,7 @@ console.log(isHorseInside);
 console.log(horseAge);
 
 //LESSON-4 Code
-console.log(typeof 11150405)
+console.log(typeof 11150405);
 const STABLE_MONTHLY_FEE = 115;
 console.log(STABLE_MONTHLY_FEE); //uppercase is prefered
 
@@ -17,4 +19,7 @@ console.log(false + 2); //TYPE COERCION
 console.log("THE MONTHLY STABLE FEE IS " + STABLE_MONTHLY_FEE);
 console.log(horseName + " will be " + (horseAge +2) + " in 2 years");
 console.log(horseName + " is serving cunt at the ripe age of " + horseAge + " being a brat is a mindset " + horseName + " is a brat and cunt");
-console.log()
+
+//Week 4 Lab Here//
+console.log(`my horse's name is ${horseName} their nickname is ${horseNickName}`);
+console.log("Come stay with " + (horseName) + " A 10% discount is applied for every 3 month stay. Save $" + (STABLE_MONTHLY_FEE * 3) *.1);
