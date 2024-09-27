d59cb586a7188bbe94c67dca016a774656c804e1
Angel Hsieh <angelhsieh@Angels-MacBook-Pro-2.local>
2024-09-27 15:20:37 -0700

    adding info to stables

--- stables.js
+++ stables.js
@@ -1,9 +1,20 @@
+//LESSON-3 Code
 let horseName = "HorseMeat";
-console.log(horseName);
 console.log(horseName);
 let horseAge = 420;
 console.log(horseAge);
-let horseHairColour = "white";
-console.log(horseHairColour);
 let isHorseInside = true;
 console.log(isHorseInside);
+console.log(horseAge);
+
+//LESSON-4 Code
+console.log(typeof 11150405)
+const STABLE_MONTHLY_FEE = 115;
+console.log(STABLE_MONTHLY_FEE); //uppercase is prefered
+
+console.log(STABLE_MONTHLY_FEE * 3);
+console.log(false + 2); //TYPE COERCION
+console.log("THE MONTHLY STABLE FEE IS " + STABLE_MONTHLY_FEE);
+console.log(horseName + " will be " + (horseAge +2) + " in 2 years");
+console.log(horseName + " is serving cunt at the ripe age of " + horseAge + " being a brat is a mindset " + horseName + " is a brat and cunt");
+console.log()
